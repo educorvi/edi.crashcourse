83292dd1c0b580d8cd7a5441cde93e47f3ddf49d
Lars Walther <lwalther@novareto.de>
2022-02-02 16:50:05 +0100

    "Add viewlet: kurs-titel-viewlet"

--- src/edi/crashcourse/content/crash_kurs.py
+++ src/edi/crashcourse/content/crash_kurs.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from plone.app.textfield import RichText
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.app.textfield import RichText
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from plone.supermodel.directives import fieldset
 from zope import schema
 from zope.interface import implementer
 from plone import api as ploneapi
@@ -14,28 +17,67 @@ class ICrashKurs(model.Schema):
     """ Marker interface and Dexterity Python Schema for CrashKurs
     """
 
+    about = RichText(
+            title = "Kursbeschreibung",
+            description = "Verfassen Sie hier eine allgemeine Beschreibung des Kurses.",
+            required = True
+            )
+
+    lernziele = RichText(
+            title = "Lernziele",
+            description = "Beschreiben Sie hier die im Kurs vermittelten Kenntnisse und Fertigkeiten.",
+            required = False
+            )
+
+    sonstiges = RichText(
+            title = "Sonstige Informationen für den Lernenden",
+            required = False
+            )
+
+    skillheadline = schema.TextLine(title = "Überschrift für die Skill-Auflistung",
+            required = False,
+            default = "Kursinhalte",
+            )
+
     autoren = schema.List(title = "Kursautor:innen",
-            description = "E-Mail-Adressen der Kursautor:innen (eine E-Mail-Adresse pro Zeile.",
+            description = "E-Mail-Adressen der Kursautor:innen (eine E-Mail-Adresse pro Zeile).",
             value_type = schema.TextLine(),
             required = True
             )
 
-    text = RichText(
-            title = u"Kursinhalt",
-            description = "Dieser Text wird vor der Auflistung der Skills angezeigt.",
-            required = True
+    benutzerdaten = schema.Bool(title = "Besuchs- und Ergebnisdaten im privaten Ordner der Lernenden speichern."
             )
 
-    schlusstext = RichText(
-            title = "Schlusstext",
-            description = "Dieser Text wird nach der Auflistung der Skills angezeigt.",
-            required = False
+    dauer = schema.TextLine(title = "Kursdauer",
+            description = "Angabe zur voraussichtlichen Gesamtdauer des Kurses, z. B. ca. 10 Stunden",
+            required = True,
             )
 
-    benutzerdaten = schema.Bool(title = "Benutzerdaten speichern",
-            description = "Checkbox markieren wenn die Nutzungsdaten in den Ordnern der Benutzer\
-                           gespeichert werden sollen."
-            )
+    fieldset(
+        'images_video',
+        label='Bilder und Video',
+        fields=('courseimage', 'courseimage_caption', 'image', 'image_caption', 'videofile', 'videoembed'),
+    )
+
+    courseimage = NamedBlobImage(title=u"Titelbild",
+            description = "Bild im Querformat, erscheint in der Einzelansicht über dem Titel.", required=False)
+
+    courseimage_caption = schema.TextLine(title=u"Bildunterschrift oder Alternativtext zum Titelbild", required=False)
+
+    image = NamedBlobImage(title="Vorschaubild",
+            description = "Bild im Querformat, erscheint in der Ordnerübersicht und im Kopf des Navigationsportlets.",
+            required = False)
+  
+    image_caption = schema.TextLine(title="Bildunterschrift oder Alternativtext zum Vorschaubild", required=False)
+
+    videofile = NamedBlobFile(title=u"Video-Datei",
+                          description=u"Hochladen einer Videodatei als Kursvorschau im *.mp4 Format.",
+                          required=False)
+
+    videoembed = schema.Text(title=u"Einbettungscode einer Videoplattform",
+                        description=u"Als Alternative zur Datei kann hier der Einbettungscode einer Videoplattform\
+                                    z.B. YouTube, Vimeo eingetragen werden.",
+                        required=False)
 
 @implementer(ICrashKurs)
 class CrashKurs(Container):
