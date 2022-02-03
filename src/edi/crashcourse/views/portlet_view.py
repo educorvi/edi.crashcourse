897699dcc58998069d02d17e93d0d578c2d266ac
Lars Walther <lwalther@novareto.de>
2022-02-03 22:24:40 +0100

    Portlet Template

--- src/edi/crashcourse/views/portlet_view.py
+++ src/edi/crashcourse/views/portlet_view.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 from edi.crashcourse import _
 from Products.Five.browser import BrowserView
+from plone import api as ploneapi
+
 
 class PortletView(BrowserView):
 
     def __call__(self):
         self.fc = self.context.getFolderContents()
+        self.kursautoren = self.getCourseTeam()
+        self.dauer = self.context.dauer
         return self.index()
+
+    def getCourseTeam(self):
+        kursautoren = []
+        autoren = self.context.autoren
+        for username in autoren:
+            entry = {}
+            user = ploneapi.user.get(username=username)
+            if user:
+                entry['fullname'] = user.getProperty('fullname')
+                entry['email'] = user.getProperty('email')
+                if entry:
+                    kursautoren.append(entry)
+        return kursautoren
