e8ab46a8867f76d02c03bfbace7fc32ea91a0a37
Lars Walther <lwalther@novareto.de>
2022-08-15 22:39:10 +0200

    Imagezoom für Crashkurs

--- src/edi/crashcourse/viewlets/kurs_titel_viewlet.py
+++ src/edi/crashcourse/viewlets/kurs_titel_viewlet.py
@@ -5,6 +5,8 @@ from plone.app.layout.viewlets import ViewletBase
 class KursTitelViewlet(ViewletBase):
 
     def render(self):
+        imageurl = self.context.absolute_url() + '/@@images/courseimage'
+        self.styleattr = f"background-image: url({imageurl});"
         if self.context.courseimage:
             return super(KursTitelViewlet, self).render()
         return ''
